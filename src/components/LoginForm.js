787130886a7a3ff46b718f085b59da2be0dc5fe3
V10LET <violet.wmoon@gmail.com>
2018-10-30 14:58:16 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,76 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+        username: '',
+        password: ''
+    };
   }
+
+  handleChange = (event) => {
+      this.setState({
+          [event.target.name]: event.target.value
+      })
+  }
+  //----> 1 destructuring in the arguments
+  handleSubmit = (event) => {
+      event.preventDefault()
+      if (this.state.username === '' || this.state.password === '') {}
+      else {
+          this.props.onSubmit(this.state)
+      }
+  }
+
+
+   //----> 2 destructure before passing in
+   // handleSubmit = (event) => {
+   //     event.preventDefault()
+   //     const { username, password } = this.state;
+   //     this.props.onSubmit(username, password)
+   // }
+   //
+   // login = (username, password) => {
+   //    console.log(`Logging in ${username} with password ${password}`);
+   //  };
+
+    //----> 3 Pass in as object
+    // handleSubmit = (event) => {
+    //     event.preventDefault()
+    //     const { username, password } = this.state;
+    //     this.props.onSubmit({
+    //         username: this.state.username,
+    //         password: this.state.password
+    //     })
+    // }
+    //
+    // login = ({ username, password }) => {
+    //    console.log(`Logging in ${username} with password ${password}`);
+    //  };
+
+     //----> 4 No destructuring at all
+     // handleSubmit = (event) => {
+     //     event.preventDefault()
+     //     const { username, password } = this.state;
+     //     this.props.onSubmit(this.state)
+     // }
+     //
+     // login = (state) => {
+     //    console.log(`Logging in ${state.username} with password ${state.password}`);
+     //  };
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleChange}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handleChange}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,26 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  state = {
+        message: '',
+        count: this.props.maxChars
+    }
 
-    this.state = {};
+
+  handleChange = (event) => {
+      this.setState({
+          message: event.target.value,
+          count: this.state.count - 1
+
+      })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.message} onChange={this.handleChange} />
+        <div>{this.state.count}</div>
       </div>
     );
   }
